9207dc6906d7adeaa14bc743fa8aaf67c8fdac47
vvozvv <a.vvozvv@gmail.com>
2021-03-30 12:57:22 +0300

    Change .env db way

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const GamesRoute = require('./routes/games')
 const MenuRoute = require('./routes/menu')
 const UserRoute = require('./routes/user')
 
-const PORT = process.env.APP_PORT || 5000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
@@ -29,7 +29,6 @@ require('./middleware/passport')(passport)
 app.use(cors())
 app.use(function(req, res) {
   res.header('Access-Control-Allow-Headers', true);
-  res.header('Access-Control-Allow-Credentials', true);
 })
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
@@ -45,13 +44,14 @@ app.use(UserRoute)
 
 async function start() {
   try {
-    await mongoose.connect(`${process.env.DATABASE_URL}`, { 
+    await mongoose.connect(`${process.env.MONGO_URI}`, { 
       useFindAndModify: false, 
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     })
 
     // start server
-    app.listen(process.env.APP_PORT, () => {
+    app.listen(process.env.PORT, () => {
       console.log('Server has been started...')
     })
   } catch(e) {
